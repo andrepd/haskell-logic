26354e042dd8d8a0e183dc7975e46c7d79856db3
andrepd <andrepd@protonmail.com>
2018-01-17 05:26:57 +0000

    "unary map" and "binary map" on Formulas. truth tables. needs cleanup.

--- logic.hs
+++ logic.hs
@@ -1,6 +1,15 @@
+{-# LANGUAGE ParallelListComp #-}
+
 import Logic.Parser
 import Logic.AST
 
+import Data.Set (Set)
+import Data.Functor
+import Data.List
+
+import Debug.Trace(traceShow, traceShowId)
+
+-- TODO make monadic (Maybe)
 type Valuation = String -> Bool
 
 eval :: Formula -> Valuation -> Bool
@@ -13,11 +22,42 @@ eval f v = case f of
     Imp x y -> not (eval x v) || eval y v
     Iff x y -> eval x v == eval y v
 
+-- instance Functor Formula where
+--     fmap func f = case f of
+--         Var x -> Var func x
+--         Not x -> Not (fmap func x)
+--         And x y -> And (fmap func x) (fmap func y)
+--         Or x y -> Or (fmap func x) (fmap func y)
+--         Imp x y -> Imp (fmap func x) (fmap func y)
+--         Iff x y -> Iff (fmap func x) (fmap func y)
+--         _ -> f
+unmap func f = case f of
+    Var x -> Var (func x)
+    Not x -> Not (unmap func x)
+    And x y -> And (unmap func x) (unmap func y)
+    Or x y -> Or (unmap func x) (unmap func y)
+    Imp x y -> Imp (unmap func x) (unmap func y)
+    Iff x y -> Iff (unmap func x) (unmap func y)
+    _ -> f
+
+-- instance Applicative Formula where
+--     pure 
+--     liftA2 
+-- binmap :: (a -> a -> a) -> a -> Formula -> a
+binmap func acc f = traceShow (acc, f) $ case f of
+    Val x -> acc
+    Var x -> func x acc
+    Not x -> binmap func acc x
+    And x y -> binmap func (binmap func acc x) y
+    Or x y -> binmap func (binmap func acc x) y
+    Imp x y -> binmap func (binmap func acc x) y
+    Iff x y -> binmap func (binmap func acc x) y
+
 prettyPrint :: Formula -> String
 prettyPrint f = case f of 
     Val x -> case x of
         True -> "T"
-        False -> "False"
+        False -> "F"
     Var x -> x
     Not x -> "~ " ++ prettyPrint x
     And x y -> "(" ++ prettyPrint x ++ " & " ++ prettyPrint y ++ ")"
@@ -25,10 +65,76 @@ prettyPrint f = case f of
     Imp x y -> "(" ++ prettyPrint x ++ " > " ++ prettyPrint y ++ ")"
     Iff x y -> "(" ++ prettyPrint x ++ " = " ++ prettyPrint y ++ ")"
 
+
+
+alistToFunc :: (Eq a) => [(a,b)] -> (a -> b)
+alistToFunc [] _ = error "Key miss"
+alistToFunc ((key,val):xs) x = if key == x then val else alistToFunc xs x
+
+-- truthTable :: Formula -> String
+-- truthTable f = [ eval v f | v <- valuations tokens ]
+--     where valuations = [  ]
+--           tokens = getTokens f
+
+truthTable :: Formula -> String
+truthTable f = let tokens = getTokens f
+                   vals = possibleValuations tokens
+                   -- rows = map (\l -> map snd l) vals
+                   rows = map (map snd) vals
+                   valsAsFuncs = map alistToFunc vals
+                   res = [eval f v | v <- valsAsFuncs]
+
+                   show' x = case x of
+                       True -> "T"
+                       False -> "F"
+                   nDashes = 2*(length $ head rows) + 3
+               -- in show rows ++ "\n" ++ show res ++ "\n" ++ show tokens
+               -- in intersperse "    " [show rows, show res, show tokens]
+               in intercalate " " tokens ++ "\n"
+                  -- ++ "--------\n"
+                  ++ replicate nDashes '-' ++ "\n"
+                  -- ++ intercalate "\n" (map (intercalate " ") (map (map show) rows)) 
+                  ++ intercalate "\n" [ row ++ " | " ++ result | row <- map (intercalate " ") (map (map show') rows) | result <- map show' res ]
+
+{-
+["y"]
+[ [ (y,True) ], [ (y,False) ] ]
+-}
+
+possibleValuations :: [String] -> [[(String,Bool)]]
+possibleValuations [] = [[]]
+-- possibleValuations (x:xs) = ((x, True):(possibleValuations xs)):((x, False):(possibleValuations xs)):xs
+-- possibleValuations (x:xs) = ((x, True):[ head x | x <- (possibleValuations xs)]):((x, False):(possibleValuations xs)):xs
+-- possibleValuations (x:xs) = let res = [(x, True):v | v <- possibleValuations xs] ++ [(x, False):v | v <- possibleValuations xs]
+--                             in traceShow (x,xs,res) res
+-- possibleValuations (x:xs) = let res = [(x,True):v | v <- valsxs] ++ [(x,False):v | v <- valsxs] where valsxs = possibleValuations xs
+--                             in traceShow (x,xs,res) res
+possibleValuations (x:xs) = [(x,True):v | v <- valsxs] ++ [(x,False):v | v <- valsxs] where valsxs = possibleValuations xs
+
+
+
+getTokens :: Formula -> [String]
+getTokens = binmap (:) []
+-- getTokens = binmap (\x y -> [x] ++ y) []
+-- getTokens = binmap (++) []
+-- getTokens f = case f of
+--     Val x -> []
+--     Var x -> [x]
+--     Not x -> getTokens x
+--     And x y -> getTokens x ++ getTokens y
+--     Or x y -> getTokens x ++ getTokens y
+--     Imp x y -> getTokens x ++ getTokens y
+--     Iff x y -> getTokens x ++ getTokens y
+
 ----
 
 main = do
-    let exp = parseExpr "x | T &y"
+    let parserResult = parseExpr "(x | F)&y"
+    let Right exp = parserResult
     putStrLn $ show $ exp
-    let Right x = exp
-    putStrLn $ prettyPrint x
+    putStrLn $ prettyPrint exp
+    let tokens = getTokens exp
+    putStrLn $ show $ tokens
+    putStrLn $ show $ possibleValuations tokens
+    -- putStrLn $ show $ possibleValuations ["x"]
+    putStrLn $ truthTable exp
--- logic_parser.hs
+++ logic_parser.hs
@@ -8,6 +8,7 @@ import Text.Parsec.Token as Token
 import Text.Parsec.Language
 -- import qualified Text.Parsec.Lexer as L
 
+-- Language definition
 def = emptyDef { identStart = letter
                , identLetter = alphaNum
                , opStart = oneOf "&|>="
@@ -16,7 +17,10 @@ def = emptyDef { identStart = letter
                , reservedNames = ["T","F"]
                }
 
+-- Lexer based on that definition
 lexer = makeTokenParser def
+
+-- Parsers
 
 m_parens = Token.parens lexer
 m_reserved = Token.reserved lexer
@@ -35,6 +39,7 @@ literalTerm = (m_reserved "T" >> return (Val True))
 
 identifierTerm = Var <$> m_identifier
 
+-- Operator table, from biggest to lowest priority
 table = [ [ prefix "-" Not ]
         , [ binary "&" And AssocLeft ]
         , [ binary "|" Or AssocLeft ]
@@ -46,5 +51,6 @@ binary  name fun assoc = Infix   (m_reservedOp name >> return fun ) assoc
 prefix  name fun       = Prefix  (m_reservedOp name >> return fun )
 postfix name fun       = Postfix (m_reservedOp name >> return fun )
 
+-- Parser
 parseExpr :: String -> Either ParseError Formula
 parseExpr = parse expr ""
